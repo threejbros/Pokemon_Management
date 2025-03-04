7301fa49663f1a96ead5d8c926d2ea1e5b17dcb1
Jonathan Lee <threejbros@gmail.com>
2025-03-04 08:48:42 -0800

    5.5 remove pokemon and its moves

--- frontend/src/components/pokemonTable.tsx
+++ frontend/src/components/pokemonTable.tsx
@@ -3,39 +3,53 @@ import Tooltip from '@mui/material/Tooltip'
 import { useGetAllIndividualPokemonsQuery, useRemovePokemonMutation } from '../services/pokemon';
 import { Pokemon } from '../types/Pokemon';
 import { useGetTrainerForSpecificPokemonQuery } from '../services/trainer';
-import { useGetPokemonMovesQuery } from '../services/pokemonMoves';
+import { useDeleteMoveMutation, useGetPokemonMovesQuery } from '../services/pokemonMoves';
 
 const PokemonRowWithMoves: React.FC<{pokemon: Pokemon; trainerName: string}> = ({pokemon, trainerName}) => {
-     // Now, we need to get the pokemon moves.
-     const {data: movesData, error: movesError, isLoading: isPokemonMovesLoading} = useGetPokemonMovesQuery(pokemon._id);
-     // If the pokemon has no moves, then disable the dropdown and show "No moves available."
-     // Otherwise if there are moves, show all the options in the dropdown.
-     const moveOptions = movesData?.allPokemonMoves?.length 
-         ? movesData?.allPokemonMoves.map((move) => (
-             <option key={move._id} value={move.name}>{move.name}</option>))
-         : <option key="nomoves" disabled>No Moves Available</option>;
-     // We also need to resolve the scenarios where loading is happening and when an error occurs.
-     // If we don't do this then if loading or an error occurs, then the dropdown could break or be empty.
-     // So, if there is loading or error, put the corresponding message
-     const moves = (isPokemonMovesLoading) ? (<span>Loading moves...</span>)
-             : (movesError ? <span>Error Loading</span> : (
-                 <select>
-                     <option value="">Show Moves</option>
-                     {moveOptions}
-                 </select>));
+    // Now, we need to get the pokemon moves.
+    const {data: movesData, error: movesError, isLoading: isPokemonMovesLoading} = useGetPokemonMovesQuery(pokemon._id);
 
     const [deletePokemon] = useRemovePokemonMutation();
+    const [deleteMove] = useDeleteMoveMutation();
+    
+
+    // If the pokemon has no moves, then disable the dropdown and show "No moves available."
+    // Otherwise if there are moves, show all the options in the dropdown.
+    const moveOptions = movesData?.allPokemonMoves?.length
+        ? movesData?.allPokemonMoves
+           .filter((move) => move !== null && move !== undefined) 
+           .map((move) => (<option key={move._id} value={move.name}>{move.name}</option>))
+        : <option key="nomoves" disabled>No Moves Available</option>;
+    // We also need to resolve the scenarios where loading is happening and when an error occurs.
+    // If we don't do this then if loading or an error occurs, then the dropdown could break or be empty.
+    // So, if there is loading or error, put the corresponding message
+    const moves = (isPokemonMovesLoading) ? (<span>Loading moves...</span>)
+            : (movesError ? <span>Error Loading</span> : (
+                <select>
+                    <option value="">Show Moves</option>
+                    {moveOptions}
+                </select>));
+
     
     const handleDelete = async () => {
         try {
-            console.log('attempting to delete pokemon')
+            // First delete the pokemon
             await deletePokemon(pokemon._id).unwrap();
             console.log(`Deleted Pokemon with ID ${pokemon._id}`);
+            // Next, delete all moves related to that pokemon 
+            if(movesData?.allPokemonMoves && movesData.allPokemonMoves.length) {
+                await Promise.all(
+                    movesData.allPokemonMoves
+                        .filter((moves) => moves !== null && moves !== undefined)
+                        .map((move) => deleteMove(move._id))
+                );
+                console.log(`Deleted all Moves associated with Pokemon ID ${pokemon._id}`);
+            }
         }
         catch (err) {
             console.log('Error: failed to delete Pokemon: ', err);
         }
-    }
+    };
      
      return (
          <tr>
@@ -80,8 +94,9 @@ export const PokemonTable: React.FC = () => {
     }
 
     const pokemonRows = data?.allPokemons?.length
-        ? data?.allPokemons?.map((pokemon) => (
-            <PokemonRow key={pokemon._id} pokemon={pokemon} />))
+        ? data?.allPokemons
+            .filter((pokemon) => pokemon !== null && pokemon !== undefined)
+            .map((pokemon) => (<PokemonRow key={pokemon._id} pokemon={pokemon} />))
         : (<tr><td colSpan={7}>No Pokemons Available. Create Pokemons First.</td></tr>)
 
     return (
